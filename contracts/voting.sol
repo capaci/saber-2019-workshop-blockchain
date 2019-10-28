44be0b6dbc45647289bc791411ce185238a5a448
Rafael capaci <rafaelcapacipereira@gmail.com>
2019-10-28 18:16:24 -0300

    Adiciona contrato gabarito

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -22,16 +22,40 @@ contract Voting {
 
     // Create a new voting with two options
     constructor(string memory _question, string memory _answer0, string memory _answer1) public {
+        owner = msg.sender;
+        question = _question;
+        answers.push(_answer0);
+        answers.push(_answer1);
     }
 
     // Finish a voting
     function finish() public {
+        require(msg.sender == owner, "Only owner can finish the voting");
+        opened = false;
+        emit Finish();
     }
 
     // Give a vote for an answer
     function vote(uint8 _answer) public {
+        require(opened == true, "Voting is closed");
+        require(_answer == 0 || _answer == 1, "Invalid option!");
+
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "You already voted");
+        sender.voted = true;
+        sender.vote = _answer;
+        total++;
+        counter[_answer]++;
+
+        emit Vote(msg.sender, _answer);
     }
 
     function winningAnswer() public view returns (int _winningAnswer) {
+        uint a = counter[0];
+        uint b = counter[1];
+
+        if (a > b) return 0;
+        if (b > a) return 1;
+        return -1;
     }
 }
